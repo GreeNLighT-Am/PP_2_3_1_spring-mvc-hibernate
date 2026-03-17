e2582daebe9f4f71ebd9b200827ca7ef096bede9
Vladislav Gutnik <lt38igreen@gmail.com>
2026-03-17 13:07:34 +0300

    Add dao, edit services, edit db.props

--- src/main/java/ru/greenlight/springmvchibernate/dao/CarDao.java
+++ src/main/java/ru/greenlight/springmvchibernate/dao/CarDao.java
@@ -1,4 +1,11 @@
 package ru.greenlight.springmvchibernate.dao;
 
+import ru.greenlight.springmvchibernate.models.Car;
+
+import java.util.List;
+
 public interface CarDao {
+    List<Car> getAllCars();
+
+    List<Car> getCarsByCount(List<Car> cars, int count);
 }
--- src/main/java/ru/greenlight/springmvchibernate/service/CarServiceImp.java
+++ src/main/java/ru/greenlight/springmvchibernate/service/CarServiceImp.java
@@ -1,29 +1,29 @@
 package ru.greenlight.springmvchibernate.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import ru.greenlight.springmvchibernate.dao.CarDao;
 import ru.greenlight.springmvchibernate.models.Car;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class CarServiceImp implements CarService {
 
+    private final CarDao carDao;
+
+    @Autowired
+    public CarServiceImp(CarDao carDao) {
+        this.carDao = carDao;
+    }
+
     @Override
     public List<Car> getAllCars() {
-        return List.of(
-                new Car("Corolla", "E150", "Silver"),
-                new Car("Audi", "A8", "Black"),
-                new Car("Subaru", "Impreza", "Blue"),
-                new Car("Toyota", "Camry", "Black"),
-                new Car("Toyota", "Prado", "Black")
-        );
+        return carDao.getAllCars();
     }
 
     @Override
     public List<Car> getCarsByCount(List<Car> cars, int count) {
-        return cars.stream()
-                .limit(count)
-                .collect(Collectors.toList());
+        return carDao.getCarsByCount(cars, count);
     }
 }
